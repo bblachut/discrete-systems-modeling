3876a639f038ec50193e19e3aa6ade437512d9ee
bblachut1 <bogojedi1@gmail.com>
2022-03-09 17:09:42 +0100

    lab2 solved

--- src/java/SoundWave/Point.java
+++ src/java/SoundWave/Point.java
@@ -10,10 +10,15 @@ public class Point {
 	public float eVel;
 	public float wVel;
 	public float sVel;
-	public float pressure;
+	private float pressure;
+	public static Integer[] types ={0,1,2};
+	public int type;
+	public int sinInput = 0;
+
 
 	public Point() {
 		clear();
+		type = 0;
 	}
 
 	public void clicked() {
@@ -21,15 +26,30 @@ public class Point {
 	}
 	
 	public void clear() {
-		// TODO: clear velocity and pressure
+		pressure = 0;
+		nVel = 0;
+		eVel = 0;
+		wVel = 0;
+		sVel = 0;
 	}
 
 	public void updateVelocity() {
-		// TODO: velocity update
+		if (type == 0) {
+			nVel = nVel - (nNeighbor.getPressure() - pressure);
+			eVel = eVel - (eNeighbor.getPressure() - pressure);
+			wVel = wVel - (wNeighbor.getPressure() - pressure);
+			sVel = sVel - (sNeighbor.getPressure() - pressure);
+		}
 	}
 
 	public void updatePresure() {
-		// TODO: pressure update
+		if (type == 0) {
+			pressure = (float) (pressure - 0.5 * (nVel + sVel + wVel + eVel));
+		}else if (type == 2){
+			double radians = Math.toRadians(sinInput);
+			pressure = (float) (Math.sin(radians));
+			sinInput = (sinInput+30)%360;
+		}
 	}
 
 	public float getPressure() {
